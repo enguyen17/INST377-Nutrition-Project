8641e30a9c0bddaf0f2bd18e6135a47b230c34c1
Emily Nguyen <emilynguyen@wireless-10-104-39-112.umd.edu>
2024-05-06 23:00:31 -0400

    addAPIfunctionality

---
+++ index.js
@@ -0,0 +1,58 @@
+const supabaseClient = require('@supabase/supabase-js')
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+const port = 3000
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+
+const supabaseUrl = 'https://tchabdxeyblwtuuenvoq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjaGFiZHhleWJsd3R1dWVudm9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ1MDQzMDUsImV4cCI6MjAzMDA4MDMwNX0.u_NbXEqNxQtsopda8sF-2vwjLkl9K3kJeS4dyDu1A_M'
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey)
+
+app.get('/', (req, res) => {
+    res.sendFile('public/HomePage.html', { root: __dirname })
+})
+
+app.get('/recipes', async (req, res) => {
+    console.log('Attempting to GET recipe analysis')
+    const { data, error } = await supabase
+        .from('INST377_NutritionApp')
+        .select()
+
+    if (error) {
+        console.log("Error")
+        res.send(error)
+    } else {
+        res.send(data)
+    }
+
+    console.log('Data:', data)
+    console.log('Error:', error)
+})
+
+app.post('/recipe', async (req, res) => {
+    console.log('Adding Recipe')
+
+    console.log(req.body)
+    var recipeName = req.body.recipeName;
+    var dishType = req.body.dishType;
+    var recipeCalories = req.body.recipeCalories;
+
+    const { data, error } = await supabase
+        .from('INST377_NutritionApp')
+        .insert({ recipe_name: recipeName, dish_type: dishType, recipe_calories: recipeCalories })
+        .select()
+
+    if (error) {
+        console.log("Error")
+        res.send(error)
+    } else {
+        res.send(data)
+    }
+})
+
+app.listen(port, () => {
+    console.log('App is alive!')
+})
---
+++ public/Nutrition.js
@@ -0,0 +1,47 @@
+var host = window.location.origin;
+console.log(host)
+
+// takes user input and inputs info into edamam api, generates response
+function loadRecipeAnalysis() {
+    let ingredients = document.getElementById('ingr').value;
+    // creates array from the ingredients input - this is the data type required for edamam api call
+    const spl_ingr = ingredients.split("\n");
+
+    // use the post function to generate nutrition analysis for the user inputted recipe
+    return fetch("https://api.edamam.com/api/nutrition-details?app_id=1f0f33e9&app_key=4a6ef18a8e967ad54e716321f0c7959f", {
+        method: 'POST',
+        body: JSON.stringify({
+            "title": `${document.getElementById('recipeName').value}`,
+            "ingr": spl_ingr
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((res) => res.json())
+}
+
+// uses generated info from edamam api to populate our database
+async function createRecipeLog() {
+    console.log('Creating Recipe Log');
+
+    // calls and stores the edamam api response from the loadRecipeAnalysis function
+    recipeAnalysis = await loadRecipeAnalysis();
+
+    // uses the post function to input the desired information into the database
+    await fetch(`${host}/recipe`, {
+        method: 'POST',
+        body: JSON.stringify({
+            "recipeName": `${document.getElementById('recipeName').value}`, 
+            "dishType": recipeAnalysis.dishType[0], 
+            "recipeCalories": recipeAnalysis.calories
+        }),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        
+    })
+
+}
